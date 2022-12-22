80550b4795bee97788bd5d3215f2d792280b6886
Ahmad-045 <ahmad.amin@devsinc.com>
2022-12-23 01:22:28 +0500

    Banner Section DONE

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,16 +1,21 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
   content: [
     './public/*.html',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
+    './app/views/**/*.{erb,haml,html,slim}',
   ],
   theme: {
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+      backgroundImage: {
+        'banner-1': "url('banner-01.jpg.webp')",
+        'banner-2': "url('banner-02.jpg.webp')",
+        'banner-3': "url('banner-03.jpg.webp')",
       },
     },
   },
@@ -18,5 +23,5 @@ module.exports = {
     require('@tailwindcss/forms'),
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/typography'),
-  ]
-}
+  ],
+};
